e0b92cb8201b60d670c33ebd106423085b96dde8
Min-Uk.Lee <makerdark98@gmail.com>
2020-10-17 20:20:32 +0900

    src: fix tag url

--- js/tag.js
+++ js/tag.js
@@ -1,3 +1,14 @@
+function getParam(sname) {
+    let params = location.search.substr(location.search.indexOf("?") + 1);
+    let sval = "";
+    params = params.split("&");
+    for (let i = 0; i < params.length; i++) {
+        temp = params[i].split("=");
+        if ([temp[0]] == sname) { sval = temp[1]; }
+    }
+    return sval;
+}
+
 function moveToTag(tagName) {
     let tagElem = document.getElementById(tagName);
     let yOffset = -100;
